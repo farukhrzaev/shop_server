d77edf9c8ac35d75c87508d8f1fb899f33956ca1
farukhrzaev <farukh.rzaev@gmail.com>
2024-05-19 08:34:52 +0300

    worked with the 'products' application. Added views, corrected templates, added content using fixtures

--- shop_proj/urls.py
+++ shop_proj/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from products.views import index
 
@@ -24,5 +26,6 @@ urlpatterns = [
        path("admin/", admin.site.urls),
        path("", index, name="index"),
        path('products/', include('products.urls', namespace='products')),
-
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
