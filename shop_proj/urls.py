3a2cd21b0dc328512f295ba0e20e085d4a9ed2ce
farukhrzaev <farukh.rzaev@gmail.com>
2024-05-19 09:10:28 +0300

    Worked with the 'users' app. Added the User model and Set up the URLconfig

--- shop_proj/urls.py
+++ shop_proj/urls.py
@@ -23,9 +23,10 @@ from django.conf import settings
 from products.views import index
 
 urlpatterns = [
-       path("admin/", admin.site.urls),
-       path("", index, name="index"),
-       path('products/', include('products.urls', namespace='products')),
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
