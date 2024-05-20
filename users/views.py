1d7273b7ba2ec77c23521d37fc5c32fc87fcf1f7
farukhrzaev <farukh.rzaev@gmail.com>
2024-05-20 04:25:15 +0300

    App Users. Added and configured templates, forms and views to customize the shopping cart

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from users.models import User
+from products.models import Basket
 
 
 def login(request):
@@ -34,6 +36,7 @@ def registration(request):
     return render(request, template_name='users/registration.html', context=context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -42,7 +45,12 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Store - Профиль', 'form': form}
+
+    context = {
+        'title': 'Store - Профиль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'users/profile.html', context)
 
 
